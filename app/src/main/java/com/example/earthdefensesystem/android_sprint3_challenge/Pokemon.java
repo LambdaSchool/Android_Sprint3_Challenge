7ca51a73ed89ca6115b514ead530173d2b97796e
JTyzz <jtyzzer@gmail.com>
2018-11-30 08:23:55 -0800

    Jason Tyzzer- Android Sprint Challenge 3

---
+++ app/src/main/java/com/example/earthdefensesystem/android_sprint3_challenge/Pokemon.java
@@ -0,0 +1,9 @@
+package com.example.earthdefensesystem.android_sprint3_challenge;
+
+import java.io.Serializable;
+
+public class Pokemon implements Serializable {
+    private static final long id = -1;
+
+
+}
