494a1dc704eb1200b0c0f49a6cdba36136e5d43f
ThaDocIzN <rodan72@msn.com>
2018-11-30 13:07:23 -0600

    started on the ui

--- SprintChallengeThree/app/src/main/java/com/thadocizn/sprintchallengethree/PokemonDao.java
+++ SprintChallengeThree/app/src/main/java/com/thadocizn/sprintchallengethree/PokemonDao.java
@@ -10,12 +10,11 @@ public class PokemonDao {
     private static final String All_POKEMON = "https://pokeapi.co/api/v2/pokemon/";
     public static ArrayList<Pokemon>getAllPokemons(){
         ArrayList<Pokemon> pokemons = new ArrayList<>();
-        String result = NetworkAdapter.httpGetRequest(All_POKEMON);
+        String page = NetworkAdapter.httpGetRequest(All_POKEMON);
         try {
-            JSONObject json = new JSONObject(result);
-            JSONArray jsonArray = json.names();
+            JSONObject json = new JSONObject(page);
+            JSONArray jsonArray = json.getJSONArray("results");
             for (int i = 0; i < jsonArray.length(); i++) {
-                String pokemon = jsonArray.getString(i);
                 pokemons.add(new Pokemon(jsonArray.getJSONObject(i)));
             }
         } catch (JSONException e) {
