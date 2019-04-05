1576dd023ffc09c51e99a75a8ed94b2efd639e08
BL-CoderOvertime <brandonlively@live.com>
2019-04-05 12:01:17 -0400

    Created Pokemon Dao

--- app/src/main/java/com/example/sprint3_pokedex/Pokemon.java
+++ app/src/main/java/com/example/sprint3_pokedex/Pokemon.java
@@ -4,16 +4,14 @@ public class Pokemon {
 
     private String[] elementType;
     private String imageURL;
-    private String key;
     private int number;
     private String name;
     private String[] moves;
     private boolean isFavorite = false;
 
-    public Pokemon(String[] elementType, String imageURL, String key, int number, String name, String[] moves) {
+    public Pokemon(String[] elementType, String imageURL, int number, String name, String[] moves) {
         this.elementType = elementType;
         this.imageURL = imageURL;
-        this.key = key;
         this.number = number;
         this.name = name;
         this.moves = moves;
@@ -33,10 +31,6 @@ public class Pokemon {
 
     public String getImageURL() {
         return imageURL;
-    }
-
-    public String getKey() {
-        return key;
     }
 
     public int getNumber() {
---
+++ app/src/main/java/com/example/sprint3_pokedex/PokemonDao.java
@@ -0,0 +1,68 @@
+package com.example.sprint3_pokedex;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class PokemonDao {
+
+
+    private static String BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
+    private static String URL_ENDING = "/.json";
+    private static String READ_POKEMON_BY_NUM = BASE_URL + "/%d" + URL_ENDING;
+
+    public static ArrayList<Pokemon> getPokemon(int num) {
+
+        String[] elementType = {"", ""};
+        String imageURL = "";
+        int number = num;
+        String name = "";
+        String[] moves = null;
+        final ArrayList<Pokemon> resultList = new ArrayList<>();
+
+        final String result = NetworkAdapter.httpRequest(String.format(READ_POKEMON_BY_NUM, num));
+        try {
+            JSONObject jsonObject = new JSONObject(result);
+
+            try {
+                name = jsonObject.getJSONObject("forms").getString("name");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                imageURL = jsonObject.getJSONObject("sprites").getString("front_default");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                JSONArray jsonArray = jsonObject.getJSONArray("types");
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    elementType[i] = jsonArray.getJSONObject(i).getJSONObject("type").getString("name");
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                JSONArray jsonArray = jsonObject.getJSONArray("moves");
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    moves[i] = jsonArray.getJSONObject(i).getJSONObject("move").getString("name");
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            resultList.add(new Pokemon(elementType, imageURL, number, name, moves));
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return resultList;
+    }
+}
