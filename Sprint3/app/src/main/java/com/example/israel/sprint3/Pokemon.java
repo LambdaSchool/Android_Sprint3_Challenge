5244d623733d99824fdbadc98994c4083a6957e9
Israel dela Cruz <igdcruz42@gmail.com>
2019-04-05 10:13:53 -0500

    added incomplete Pokemon class

---
+++ Sprint3/app/src/main/java/com/example/israel/sprint3/Pokemon.java
@@ -0,0 +1,77 @@
+package com.example.israel.sprint3;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class Pokemon {
+
+    public Pokemon(JSONObject pokemonJson) {
+        // id
+        try {
+            this.id = pokemonJson.getInt("id");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //name
+        try {
+            this.name = pokemonJson.getString("name");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // sprite url
+        try {
+            JSONObject spritesJson = pokemonJson.getJSONObject("sprites");
+            this.spriteUrl = spritesJson.getString("front_default");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // abilities
+        try {
+            JSONArray abilityInfoJsonArr = pokemonJson.getJSONArray("abilities");
+            // TODO
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // types
+        try {
+            JSONArray typeInfoJsonArr = pokemonJson.getJSONArray("types");
+            // TODO
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int id;
+    private String name;
+    private String spriteUrl;
+    private ArrayList<String> abilities;
+    private ArrayList<String> types;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpriteUrl() {
+        return spriteUrl;
+    }
+
+    public ArrayList<String> getAbilities() {
+        return abilities;
+    }
+
+    public ArrayList<String> getTypes() {
+        return types;
+    }
+
+}
