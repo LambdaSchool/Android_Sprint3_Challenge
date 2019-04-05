9ef197cee990e99c3e797eaf72ef2c45fc8c47a8
Israel dela Cruz <igdcruz42@gmail.com>
2019-04-05 10:21:04 -0500

    added pokemon search result recycler view

--- Sprint3/app/src/main/java/com/example/israel/sprint3/MainActivity.java
+++ Sprint3/app/src/main/java/com/example/israel/sprint3/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.israel.sprint3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText pokemonSearchEditText = findViewById(R.id.edit_text_pokemon_search);
+        pokemonSearchEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        Button pokemonSearchButton = findViewById(R.id.button_pokemon_search);
+        pokemonSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
