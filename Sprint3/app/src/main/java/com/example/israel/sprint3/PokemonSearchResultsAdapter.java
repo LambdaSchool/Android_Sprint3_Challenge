667c422aa09a8cdaf3cb54d9813b4e66d3370a70
Israel dela Cruz <igdcruz42@gmail.com>
2019-04-05 10:25:47 -0500

    added PokemonSearchResultsAdapter

---
+++ Sprint3/app/src/main/java/com/example/israel/sprint3/PokemonSearchResultsAdapter.java
@@ -0,0 +1,51 @@
+package com.example.israel.sprint3;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class PokemonSearchResultsAdapter extends RecyclerView.Adapter<PokemonSearchResultsAdapter.ViewHolder> {
+
+    private ArrayList<String> pokemonNames;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return new ViewHolder(LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.pokemon_search_result_layout, viewGroup, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        String pokemonName = pokemonNames.get(i);
+
+        viewHolder.pokemonNameTextView.setText(pokemonName);
+
+        viewHolder.pokemonNameTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO pokemon details
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return pokemonNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            pokemonNameTextView = itemView.findViewById(R.id.text_view_pokemon_name);
+        }
+
+        TextView pokemonNameTextView;
+    }
+}
