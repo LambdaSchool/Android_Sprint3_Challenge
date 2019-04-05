0ec2d98c7d6eb93fc44b74af590c20eb37aaa8dc
BL-CoderOvertime <brandonlively@live.com>
2019-04-05 11:35:30 -0400

    setUp favorites list xml layout

---
+++ app/src/main/java/com/example/sprint3_pokedex/PokeFavoritesListActivity.java
@@ -0,0 +1,13 @@
+package com.example.sprint3_pokedex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PokeFavoritesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poke_favorites_list);
+    }
+}
